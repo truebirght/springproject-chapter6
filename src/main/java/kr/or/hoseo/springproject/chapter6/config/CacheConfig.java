657562b3f0203d8bdd1871fa53ac2dbe9cdacdce
pjh0410 <pjh0410@gmail.com>
2018-06-02 09:46:32 +0900

    redis cache manager 적용

--- src/main/java/kr/or/hoseo/springproject/chapter6/config/CacheConfig.java
+++ src/main/java/kr/or/hoseo/springproject/chapter6/config/CacheConfig.java
@@ -1,17 +1,44 @@
 package kr.or.hoseo.springproject.chapter6.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+	@Value("${redis.host}")
+	private String host;
 
+	@Value("${redis.port}")
+	private int port;
+	
 	@Bean
 	public CacheManager cacheManager() {
-		return new ConcurrentMapCacheManager();
+		return new RedisCacheManager(redisTemplate());
+	}
+	
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory factory = new JedisConnectionFactory();
+		factory.setHostName(host);
+		factory.setPort(port);
+		factory.setUsePool(true);
+		
+		return factory;
+	}
+	
+	@Bean
+	public RedisTemplate<Object, Object> redisTemplate(){
+		RedisTemplate<Object, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
 	}
 }
